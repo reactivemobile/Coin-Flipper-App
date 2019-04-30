00f9c0ef4fbde42c47bbddf3d1ff70fc99723f7c
Donal OCallaghan <donal@reactivemobile.com>
2019-04-30 23:23:57 +0100

    Renaming app ID

--- app/src/androidTest/java/com/reactivemobile/app/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/reactivemobile/app/ExampleInstrumentedTest.kt
@@ -2,11 +2,9 @@ package com.reactivemobile.app
 
 import androidx.test.InstrumentationRegistry
 import androidx.test.runner.AndroidJUnit4
-
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -19,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.reactivemobile.whitelabelapp", appContext.packageName)
+        assertEquals("com.reactivemobile.coinflipperapp", appContext.packageName)
     }
 }
